7c15c8629668c2d0840678a7140c738a12503509
BazimAzeem <72491752+BazimAzeem@users.noreply.github.com>
2021-08-10 08:23:15 +0530

    Center the canvas

--- sketch.js
+++ sketch.js
@@ -8,8 +8,11 @@ var numBalls = 7;
 var frameOffset = 15;
 
 function setup() {
-  // frameRate(5);
-  createCanvas(500, 500);
+  var cnv = createCanvas(500, 500);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);  rectMode(CENTER);
+  
   angleMode(DEGREES);
 
   ballWidth = width / 15;
